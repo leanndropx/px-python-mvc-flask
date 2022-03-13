c10e95862ac70f0edc6a2c26bd49a6ee31598cde
leanndropx <leanndrompeixoto1@gmail.com>
2022-03-13 17:10:27 -0300

    exercitando framework

--- app_dir/controller/__init__.py
+++ app_dir/controller/__init__.py
@@ -13,3 +13,10 @@ def contatos():
     search = request.args.get("search")
     return render_template("resultado.html",search=search)
     
+@app.route("/contato")
+def contato():
+    return render_template("contato.html")
+
+@app.route("/quemsomos")
+def quemsomos():
+    return render_template("quemsomos.html")
